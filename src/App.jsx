3c958edb3d6961cdbd09f94429cd4366f405057e
Thomas Emerson <emersonvisuals@gmail.com>
2023-02-27 11:08:00 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import "./styles/main.scss";
+import Spline from "@splinetool/react-spline";
+import Logo from "./images/logo.png";
+import Instagram from "./images/instagram.svg";
+import Facebook from "./images/facebook.svg";
+import Linkedin from "./images/linkedin.svg";
+import { useState } from "react";
+
+function App() {
+    const [toggleMenu, setToggleMenu] = useState(true);
+
+    const handleToggleMenu = () => {
+        setToggleMenu(!toggleMenu);
+    };
+
+    return (
+        <div className="App">
+            <Spline scene="https://prod.spline.design/KIHUKysh-BGuqpT0/scene.splinecode" />
+            <nav className={toggleMenu === true ? "" : "active"}>
+                <div className="itemsContainer">
+                    <div className="logo">
+                        <img src={Logo} alt="logo" />
+                    </div>
+                    <div className="navList">
+                        <ul>
+                            <li>
+                                <a href="/">home</a>
+                            </li>
+                            <li>
+                                <a href="/">about</a>
+                            </li>
+                            <li>
+                                <a href="/">projects</a>
+                            </li>
+                            <li>
+                                <a href="/">contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="burgerContainer">
+                        <div
+                            className="burgerTrigger"
+                            onClick={() => {
+                                handleToggleMenu();
+                            }}
+                        ></div>
+                        <div className="burgerMenu"></div>
+                    </div>
+                </div>
+            </nav>
+            <section>
+                <div className="itemsContainer">
+                    <div className="contents">
+                        <h1>
+                            Let's build <span>something epic.</span>
+                        </h1>
+                        <p>
+                            Enhancing digital product experiences for businesses
+                            in Australia and New Zealand is our specialty. Click
+                            below to learn more about our software company.
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button className="one">Get started.</button>
+                        <button className="two">Our story.</button>
+                    </div>
+                </div>
+            </section>
+            <footer>
+                <div className="contents">
+                    <ul>
+                        <li>
+                            <a href="/">
+                                <img src={Instagram} alt="instagram" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <img src={Linkedin} alt="linkedin" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <img src={Facebook} alt="facebook" />
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="creator">
+                        <p>UI created by Emerson Visuals</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default App;
